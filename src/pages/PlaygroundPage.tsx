e70eca508b9ec2a483e5c1156a5f502f2318817c
Rabia-Rajput <rajput.rabia@outlook.com>
2024-06-29 12:19:07 +0200

    update file

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,9 @@
+import ExpandableText from "../components/ExpandableText";
 import Onboarding from "../components/Onboarding";
 
 const PlaygroundPage = () => {
-  return <Onboarding />;
+
+  return <ExpandableText text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore cupiditate dicta natus quo eaque dolorem qui quisquam accusamus nesciunt debitis alias facere possimus, eum nihil expedita laboriosam earum cum eligendi  Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore cupiditate dicta natus quo eaque dolorem qui quisquam accusamus nesciunt debitis alias facere possimus, eum nihil expedita laboriosam earum cum eligendi.." />;
 };
 
 export default PlaygroundPage;
